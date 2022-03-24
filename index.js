a06d10cd53d40bc7b03019ca19d43a2b5ba3b2a0
jodu07 <jhonnat.dussan@gmail.com>
2022-03-24 01:35:46 -0500

    commit 2

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const port = (process.env.port || 3000);
 
 const bodyParser = require('body-parser');
 
-
-
 // express
 const app = express();
 
@@ -15,7 +13,6 @@ app.use(express.json());
 
 //config
 app.set('port', port);
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
--- ruta.js
+++ ruta.js
@@ -1,5 +1,17 @@
 const conexion = require('./config/conexion')
 const ruta = require('express').Router()
 
+//get players
+ruta.get('/',(req, res)=>{
+    let sql ='select * from player'
+    conexion.query(sql,(err, rows, fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows)
+        }
+    })
+})
+
+
 
 module.exports = ruta  
