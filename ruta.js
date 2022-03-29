382a24d703d7afacaf7804315780e31c82345e2a
jodu07 <jhonnat.dussan@gmail.com>
2022-03-29 01:13:23 -0500

    get por nombre

--- ruta.js
+++ ruta.js
@@ -13,5 +13,31 @@ ruta.get('/',(req, res)=>{
 })
 
 
+// get un jugador
+ruta.get('/:name',(req, res)=>{
+    const {name} = req.params
+    let sql ='select * from player where name = ?'
+    conexion.query(sql,[name],(err, rows, fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows)
+        }
+    })
+})
+
+
+ruta.get('/:id',(req, res)=>{
+    const {id} = req.params
+    let sql ='select * from player where idPlayer = ?'
+    conexion.query(sql,[id],(err, rows, fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows)
+        }
+    })
+})
+
+
+
 
 module.exports = ruta  
